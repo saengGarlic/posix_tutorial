07849b6593a98662b78b88996b6570de86741f7d
saengGarlic <jwh6290@gmail.com>
2020-10-30 10:06:38 +0900

    30OCT

---
+++ socket30OCT/server.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	
+	int server_sockfd, client_sockfd;
+	int server_len, client_len;
+	struct sockaddr_in server_address;
+	struct sockaddr_in client_address;
+	
+	unlink("server_socket");
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(9734);
+	server_len = sizeof(server_address);
+	bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+	
+	listen(server_sockfd, 5);
+	while(1){
+		char ch;
+		
+		print("server waiting\n");
+		
+		client_len = sizeof(client_address);
+		client_sockfd = accept(server_scofd, (struct sockaddr *)&client_Address, &client_len);
+		
+		read(client_sockfd, &ch, 1);
+		ch++;
+		write(client_sockfd, &ch, 1);
+		close(client_sockfd);
+	}
+}
